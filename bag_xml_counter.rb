e7a6f6a89fee79ba3eb784ceea196ad8256e9fc3
Yasuhiro Asaka <yasaka@ywesee.com>
2012-04-19 12:56:32 +0200

    Improved Zip file(argument) handling

--- bag_xml_counter.rb
+++ bag_xml_counter.rb
@@ -1,36 +1,35 @@
 #!/usr/bin/env ruby
-# -*- coding: utf-8 -*-
+# encoding: utf-8
 
 require 'zip/zip'
 require 'rexml/document'
 include REXML
 
-def report(filename, paths)
-  puts filename
-  xml = ''
-  File.open(filename, 'r') do |f|
-    xml = f.read
-  end
-  doc = Document.new(xml)
-  paths.each_pair do |key, path|
+def report(name, io, paths)
+  puts name
+  doc = Document.new(io.read)
+  paths.each_pair do |key, node|
     {
       de: 'DescriptionDe',
       fr: 'DescriptionFr'
-    }.each_pair do |lang, lang_path|
+    }.each_pair do |lang, lang_node|
       count = 0
-      doc.elements.each(path + "/" + lang_path) do |element|
+      doc.elements.each(node + "/" + lang_node) do |element|
        count += 1 if element.get_text.to_s.chomp.length > 0
       end
-      puts "#{key} [#{lang}]: #{count}"
+      puts "  #{key} [#{lang}]: #{count}"
     end
   end
 end
 
-zip = 'XMLPublications-2012.04.02.zip'
-if ARGV.first != zip or
-   !File.exist?(zip)
+unless zip = ARGV.first
+  zip = './XMLPublications.zip'
+end
+if !File.exist?(zip)
+  puts "#{zip} file not exist."
   exit
 end
+
 Zip::ZipFile.foreach(zip) do |entry|
   case entry.name
   when 'Preparations.xml'
@@ -39,55 +38,15 @@ Zip::ZipFile.foreach(zip) do |entry|
       packs:        'Preparations/Preparation/Packs/Pack/Limitations/Limitation',
       itcodes:      'Preparations/Preparation/ItCodes/ItCode/Limitations/Limitation',
     }
-    report 'Preparations.xml', paths
-  when 'ItCodes.xml'
-    #paths = {
-    #  itcode: 'ItCodes/ItCode/Limitations/Limitation',
-    #}
-    #report 'Preparations.xml', paths
-  end
-end
-
-=begin
-File.open('ItCodes.xml', 'r') do |f|
-  xml = f.read 
-end
-doc = Document.new(xml)
-
-paths = {
-  de: 'ItCodes/ItCode/Limitations/Limitation/DescriptionDe',
-  fr: 'ItCodes/ItCode/Limitations/Limitation/DescriptionFr'
-}
-paths.each_pair do |lang, path|
-  count = 0
-  doc.elements.each(path) do |element|
-   count += 1 if element.get_text.to_s.chomp.length > 0
-  end
-  p "#{lang}: #{count}"
-end
-=end
-
-=begin
-File.open('Preparations.xml', 'r') do |f|
-  xml = f.read 
-end
-doc = Document.new(xml)
-
-paths = {
-  preparations: 'Preparations/Preparation/Limitations/Limitation',
-  packs:        'Preparations/Preparation/Packs/Pack/Limitations/Limitation',
-  itcodes:      'Preparations/Preparation/ItCodes/ItCode/Limitations/Limitation',
-}
-paths.each_pair do |key, path|
-  {
-    de: 'DescriptionDe',
-    fr: 'DescriptionFr'
-  }.each_pair do |lang, lang_path|
-    count = 0
-    doc.elements.each(path + "/" + lang_path) do |element|
-     count += 1 if element.get_text.to_s.chomp.length > 0
+    entry.get_input_stream do |io|
+      report entry.name, io, paths
     end
-    p "#{key} [#{lang}]: #{count}"
+  when 'ItCodes.xml'
+    paths = {
+      itcodes: 'ItCodes/ItCode/Limitations/Limitation',
+    }
+    entry.get_input_stream do |io|
+      report entry.name, io, paths
+    end
   end
 end
-=end
